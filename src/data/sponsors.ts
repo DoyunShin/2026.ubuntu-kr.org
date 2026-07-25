377284e2b6d98ff97b2af0335776514b8e647c09
paperbox <7126454+lego37yoon@users.noreply.github.com>
2026-07-26 01:59:35 +0900

    cont: 0726 확정 후원사 추가

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -1,13 +1,17 @@
 // import logoPlaceHolder from "@assets/logo_placeholder.svg";
-// import canonicalLogo from "@assets/sponsors/ubuntu.svg";
-// import microsoftLogo from "@assets/sponsors/microsoft.png";
+import canonicalLogo from "@assets/sponsors/ubuntu.svg";
+import awsLogo from "@assets/sponsors/aws.png"
+import ksugLogo from "@assets/sponsors/ksug.png";
+import groMetricLogo from "@assets/sponsors/grometric.svg";
 
 
 export type Sponsor = {
     name: string,
     logoUrl: string,
     url: string,
-    description: string
+    description: string,
+    darkInvertBack?: boolean,
+    prevSponsored?: number,
 }
 
 type Sponsors = {
@@ -27,12 +31,14 @@ export const sponsors = [
         sizeOnSmallScreen: 4,
         showPopup: true,
         list: [
-            // {
-            //     name: "Ubuntu",
-            //     logoUrl: canonicalLogo.src,
-            //     url: "https://ubuntu.com/",
-            //     description: "Canonical은 대부분의 퍼블릭 클라우드 워크로드를 위한 OS이자 스마트 게이트웨이, 자율 주행 자동차 및 Advanced robot 등 새롭게 떠오르는 분야를 위한 OS인 Ubuntu를 개발 및 배포하는 회사입니다. Canonical은 상용 Ubuntu 사용자에게 엔터프라이즈 보안, 지원 및 서비스를 제공합니다."
-            // },
+            {
+                name: "Ubuntu",
+                logoUrl: canonicalLogo.src,
+                url: "https://ubuntu.com/",
+                prevSponsored: 6,
+                darkInvertBack: true,
+                description: "Canonical은 대부분의 퍼블릭 클라우드 워크로드를 위한 OS이자 스마트 게이트웨이, 자율 주행 자동차 및 Advanced robot 등 새롭게 떠오르는 분야를 위한 OS인 Ubuntu를 개발 및 배포하는 회사입니다. Canonical은 상용 Ubuntu 사용자에게 엔터프라이즈 보안, 지원 및 서비스를 제공합니다."
+            },
         ] as Sponsor[]
     },
     {
@@ -42,13 +48,13 @@ export const sponsors = [
         sizeOnSmallScreen: 4,
         showPopup: true,
         list: [
-            // {
-            //     name: "Microsoft Korea",
-            //     logoUrl: microsoftLogo.src,
-            //     url: "https://azure.microsoft.com/en-us/ubuntu/?ocid=AID3035196",
-            //     description: "Microsoft는 지구상의 모든 사람과 조직이 더 많은 것을 성취할 수 있도록 역량을 지원하는 것을 사명으로 삼고 있습니다. Ubuntu 기반 워크로드는 Azure에서 가장 빠르게 성장하고 있는 워크로드 중 하나입니다. Microsoft는 Canonical과 제휴하여 Azure에서 동작하는 Ubuntu와 오픈 소스 커뮤니티 참여를 통해 Azure에 최적화된 커널, 개선된 성능, 고급 보안, 자동 패치 및 업데이트, 유연한 가격 책정 옵션을 제공합니다."
-            // },
-
+            {
+                name: "Amazon Web Services Korea",
+                logoUrl: awsLogo.src,
+                url: "https://aws.amazon.com/ko/",
+                darkInvertBack: true,
+                description: "Amazon Web Services(AWS)는 전 세계적으로 분포한 데이터 센터에서 200개가 넘는 완벽한 기능의 서비스를 제공하는, 세계적으로 가장 포괄적이며, 널리 채택되고 있는 클라우드입니다. 빠르게 성장하는 스타트업, 대기업, 주요 정부 기관을 비롯한 수많은 고객이 AWS를 사용하여 비용을 절감하고, 민첩성을 향상시키고, 혁신 속도를 높이고 있습니다."
+            },
         ] as Sponsor[]
     },
     {
@@ -58,6 +64,13 @@ export const sponsors = [
         sizeOnSmallScreen: 2,
         showPopup: true,
         list: [
+            {
+                name: "GroMetric",
+                logoUrl: groMetricLogo.src,
+                url: "http://www.grometric.kr/",
+                prevSponsored: 4,
+                description: "그로메트릭은 고객의 지속가능한 성장을 위한 인텔리전스와 안전함을 제공합니다. 중장기적인 관점에서 데이터 메트릭을 통한 고객의 성장을 돕는 솔루션 개발을 목표하고 있어요. 아울러 다양한 OpenSource Software, Cloud, Security 솔루션으로 여러분의 성장과 함께 하고 싶어요."
+            },
         ] as Sponsor[]
     },
     {
@@ -76,6 +89,12 @@ export const sponsors = [
         sizeOnSmallScreen: 2,
         showPopup: true,
         list: [
+            {
+                name: "한국스프링사용자모임(KSUG)",
+                logoUrl: ksugLogo.src,
+                url: "https://www.ksug.org",
+                description: "KSUG는 스프링을 사용하거나 관심있는 사람들이 모여 뉴스와 정보를 공유하고 문제를 같이 해결하는 곳이며 스터디, 세미나, 컨퍼런스 참여 등의 오프라인 활동을 합니다."
+            }
         ] as Sponsor[]
     }
 ] satisfies Sponsors
